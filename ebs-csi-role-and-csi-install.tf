a8fecbeb624b798643bca4a92db432543076f38d
nalajala9 <nalajalaravi99@gmail.com>
2023-02-08 19:11:57 +0530

    updated

--- ebs-csi-role-and-csi-install.tf
+++ ebs-csi-role-and-csi-install.tf
@@ -1,83 +1,83 @@
 
-# Resource: Create EBS CSI IAM Policy 
-resource "aws_iam_policy" "ebs_csi_iam_policy" {
-  name        = "AmazonEKS_EBS_CSI_Driver_Policy"
-  path        = "/"
-  description = "EBS CSI IAM Policy"
-  policy = data.http.ebs_csi_iam_policy.response_body
-}
+# # Resource: Create EBS CSI IAM Policy 
+# resource "aws_iam_policy" "ebs_csi_iam_policy" {
+#   name        = "AmazonEKS_EBS_CSI_Driver_Policy"
+#   path        = "/"
+#   description = "EBS CSI IAM Policy"
+#   policy = data.http.ebs_csi_iam_policy.response_body
+# }
 
-# Resource: Create IAM Role and associate the EBS IAM Policy to it
+# # Resource: Create IAM Role and associate the EBS IAM Policy to it
 
-resource "aws_iam_role" "ebs_csi_iam_role" {
-  name = "${var.project}-ebs-csi-iam-role"
-
-
-# Terraform's "jsonencode" function converts a Terraform expression result to valo JSON syntax.
-
-assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRoleWithWeboentity"
-        Effect = "Allow"
-        So    = ""
-        Principal = {
-          Federated = "${data.terraform_remote_state.eks.outputs.aws_iam_openo_connect_provoer_arn}"
-        }
-        Condition = {
-          StringEquals = {            
-            "${data.terraform_remote_state.eks.outputs.aws_iam_openo_connect_provoer_extract_from_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
-          }
-        }        
-
-      },
-    ]
-  })
-
-  tags = {
-    tag-key = "${var.project}-ebs-csi-iam-role"
-  }
-}
-
-# Associate EBS CSI IAM Policy to EBS CSI IAM Role
-
-resource "aws_iam_role_policy_attachment" "ebs_csi_iam_role_policy_attach" {
-  policy_arn = aws_iam_policy.ebs_csi_iam_policy.arn 
-  role       = aws_iam_role.ebs_csi_iam_role.name
-}
-
-# Install EBS CSI Driver using HELM
-# Resource: Helm Release 
+# resource "aws_iam_role" "ebs_csi_iam_role" {
+#   name = "${var.project}-ebs-csi-iam-role"
 
 
-resource "helm_release" "ebs_csi_driver" {
-  depends_on = [aws_iam_role.ebs_csi_iam_role ]
-  name       = "${var.project}-aws-ebs-csi-driver"
+# # Terraform's "jsonencode" function converts a Terraform expression result to valo JSON syntax.
 
-  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
-  chart      = "aws-ebs-csi-driver"
+# assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRoleWithWeboentity"
+#         Effect = "Allow"
+#         So    = ""
+#         Principal = {
+#           Federated = "${data.terraform_remote_state.eks.outputs.aws_iam_openo_connect_provoer_arn}"
+#         }
+#         Condition = {
+#           StringEquals = {            
+#             "${data.terraform_remote_state.eks.outputs.aws_iam_openo_connect_provoer_extract_from_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+#           }
+#         }        
 
-  namespace = "kube-system"     
+#       },
+#     ]
+#   })
 
-  set {
-    name = "image.repository"
-    value = "602401143452.dkr.ecr.ap-south-1.amazonaws.com/eks/aws-ebs-csi-driver" # Changes based on Region - This is for us-east-1 Additional Reference: https://docs.aws.amazon.com/eks/latest/userguoe/add-ons-images.html
-  }       
+#   tags = {
+#     tag-key = "${var.project}-ebs-csi-iam-role"
+#   }
+# }
 
-  set {
-    name  = "controller.serviceAccount.create"
-    value = "true"
-  }
+# # Associate EBS CSI IAM Policy to EBS CSI IAM Role
 
-  set {
-    name  = "controller.serviceAccount.name"
-    value = "ebs-csi-controller-sa"
-  }
+# resource "aws_iam_role_policy_attachment" "ebs_csi_iam_role_policy_attach" {
+#   policy_arn = aws_iam_policy.ebs_csi_iam_policy.arn 
+#   role       = aws_iam_role.ebs_csi_iam_role.name
+# }
 
-  set {
-    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = "${aws_iam_role.ebs_csi_iam_role.arn}"
-  }
+# # Install EBS CSI Driver using HELM
+# # Resource: Helm Release 
+
+
+# resource "helm_release" "ebs_csi_driver" {
+#   depends_on = [aws_iam_role.ebs_csi_iam_role ]
+#   name       = "${var.project}-aws-ebs-csi-driver"
+
+#   repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+#   chart      = "aws-ebs-csi-driver"
+
+#   namespace = "kube-system"     
+
+#   set {
+#     name = "image.repository"
+#     value = "602401143452.dkr.ecr.ap-south-1.amazonaws.com/eks/aws-ebs-csi-driver" # Changes based on Region - This is for us-east-1 Additional Reference: https://docs.aws.amazon.com/eks/latest/userguoe/add-ons-images.html
+#   }       
+
+#   set {
+#     name  = "controller.serviceAccount.create"
+#     value = "true"
+#   }
+
+#   set {
+#     name  = "controller.serviceAccount.name"
+#     value = "ebs-csi-controller-sa"
+#   }
+
+#   set {
+#     name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = "${aws_iam_role.ebs_csi_iam_role.arn}"
+#   }
     
-}
+# }
